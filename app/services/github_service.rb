ff3fbd2abb9d55ab6b2f7dfa07ef5122ee8b79ed
Adam Bailey <ab67319@gmail.com>
2023-03-01 21:34:23 -0700

    refactor: add caching to minimize API calls

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -2,15 +2,15 @@ require 'httparty'
 
 class GithubService
   def repos
-    get_url("https://api.github.com/repos/aj-bailey/little-esty-shop")
+    Rails.cache.fetch([self, :repos], expires_in: 20.minutes) { get_url("https://api.github.com/repos/aj-bailey/little-esty-shop") }
   end
-
+  
   def pulls
-    get_url("https://api.github.com/repos/aj-bailey/little-esty-shop/pulls?state=closed")
+    Rails.cache.fetch([self, :pulls], expires_in: 20.minutes) { get_url("https://api.github.com/repos/aj-bailey/little-esty-shop/pulls?state=closed") }
   end
 
   def contributors
-    get_url("https://api.github.com/repos/aj-bailey/little-esty-shop/contributors")
+    Rails.cache.fetch([self, :contributors], expires_in: 20.minutes) { get_url("https://api.github.com/repos/aj-bailey/little-esty-shop/contributors") }
   end
 
   def get_url(url)
